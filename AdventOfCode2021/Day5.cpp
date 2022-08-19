5da7f5774030312a3bd68bfec05be46c92f71ef2
Mingos <domingos_forno@hotmail.com>
2022-08-19 17:01:25 +0100

    Part 2 day 5.

--- AdventOfCode2021/Day5.cpp
+++ AdventOfCode2021/Day5.cpp
@@ -6,48 +6,93 @@
 #include "Day5.h"
 
 
+
 void Day5::Solve() {
-    std::ifstream inputFile("Day5.input");
 
-    if (!inputFile.is_open())
-    {
-        std::cerr << "Error opening file." << std::endl;
-    }
-
-    std::unordered_map<Point, int, PointHasher> vents;
-
-    Line l;
-    Point p;
-
-    while (inputFile >> l)
-    {
-        if (!l.is_horizontal_or_vertical())
-            continue;
-
-        for (auto i = l.start.x; i <= l.end.x; i++)
-        {
-            for (auto j = l.start.y; j <= l.end.y; j++)
-            {
-                p = { i, j };
-                vents.emplace(p, 0);
-                ++vents[p];
-            }
-        }
-    }
-
-    int output = 0;
-
-    for (auto&& vent : vents)
-    {
-        int currentVent = vent.second;
-        if (currentVent >= 2)
-        {
-            ++output;
-        }
-    }
-
-    std::cout << "Unique intersections: " << output << "\n";
-
+	FindPoints(false);
+	FindPoints(true);
 }
 
+void Day5::FindPoints(bool flag) {
 
+	std::ifstream inputFile("Day5.input");
+
+	if (!inputFile.is_open())
+	{
+		std::cerr << "Error opening file." << std::endl;
+	}
+
+	std::unordered_map<Point, int, PointHasher> vents;
+
+	Line l;
+	Point p;
+
+
+	while (inputFile >> l)
+	{
+		if (l.is_vertical()) {
+
+			for (auto i = l.start.x; i <= l.end.x; i++)
+			{
+				p = { i,l.start.y };
+				vents.try_emplace(p, 0);
+				++vents[p];
+			}
+		}
+		else if (l.is_horizontal()) {
+			for (auto i = l.start.y; i <= l.end.y; i++)
+			{
+				p = { l.start.x, i };
+				vents.try_emplace(p, 0);
+				++vents[p];
+			}
+		}
+		else if (flag) {
+
+			if (l.start.y < l.end.y)
+			{
+				int currentX = l.start.x;
+				int currentY = l.start.y;
+
+				while (currentX <= l.end.x && currentY <= l.end.y)
+				{
+					p = { currentX, currentY };
+					vents.try_emplace(p, 0);
+					++vents[p];
+
+					++currentX;
+					++currentY;
+				}
+			}
+			else if (l.start.y > l.end.y)
+			{
+				int currentX = l.start.x;
+				int currentY = l.start.y;
+
+				while (currentX <= l.end.x && currentY >= l.end.y)
+				{
+					p = { currentX, currentY };
+					vents.try_emplace(p, 0);
+					++vents[p];
+
+					++currentX;
+					--currentY;
+				}
+			}
+		}
+	}
+
+	int output = 0;
+
+	for (auto&& vent : vents)
+	{
+		
+		int currentVent = vent.second;
+		if (currentVent >= 2)
+		{
+			++output;
+		}
+	}
+
+	std::cout << "Unique intersections: " << output << "\n";
+}
--- AdventOfCode2021/Day5.h
+++ AdventOfCode2021/Day5.h
@@ -4,77 +4,89 @@ class Day5 {
 public:
 	void Solve();
 private:
-    struct Point
-    {
-        int x, y;
+	struct Point
+	{
+		int x, y;
 
-        bool operator==(const Point& Other) const
-        {
-            return x == Other.x && y == Other.y;
-        }
+		bool operator==(const Point& Other) const
+		{
+			return x == Other.x && y == Other.y;
+		}
 
-        friend std::istream& operator>>(std::istream& s, Point& p)
-        {
-            if (!s)
-                return s;
+		friend std::istream& operator>>(std::istream& s, Point& p)
+		{
+			if (!s)
+				return s;
 
-            if (!(s >> p.x))
-                return s;
+			if (!(s >> p.x))
+				return s;
 
-            char delim = 0;
-            s >> delim;
-            if (delim != ',')
-            {
-                s.setstate(std::ios_base::badbit);
-                return s;
-            }
+			char delim = 0;
+			s >> delim;
+			if (delim != ',')
+			{
+				s.setstate(std::ios_base::badbit);
+				return s;
+			}
 
-            s >> p.y;
+			s >> p.y;
 
-            return s;
-        }
-    };
+			return s;
+		}
+	};
 
-    struct PointHasher
-    {
-        size_t operator()(const Point& Input) const
-        {
-            return std::hash<int>{}(Input.x) ^ (std::hash<int>{}(Input.y) + 0xbeef);
-        }
-    };
+	struct PointHasher
+	{
+		size_t operator()(const Point& Input) const
+		{
+			return std::hash<int>{}(Input.x) ^ (std::hash<int>{}(Input.y) + 0xbeef);
+		}
+	};
 
-    struct Line
-    {
-        Point start, end;
+	struct Line
+	{
+		Point start, end;
 
-        bool is_horizontal_or_vertical() const { return start.x == end.x || start.y == end.y; }
+		bool is_horizontal() const { return start.x == end.x; }
+		bool is_vertical() const { return start.y == end.y; }
 
-        friend std::istream& operator>>(std::istream& s, Line& line)
-        {
-            if (!s)
-                return s;
+		friend std::istream& operator>>(std::istream& s, Line& line)
+		{
+			if (!s)
+				return s;
 
-            if (!(s >> line.start))
-                return s;
+			if (!(s >> line.start))
+				return s;
 
-            char delim1 = 0, delim2 = 0;
-            s >> delim1 >> delim2;
-            if (delim1 != '-' || delim2 != '>')
-            {
-                s.setstate(std::ios_base::badbit);
-                return s;
-            }
+			char delim1 = 0, delim2 = 0;
+			s >> delim1 >> delim2;
+			if (delim1 != '-' || delim2 != '>')
+			{
+				s.setstate(std::ios_base::badbit);
+				return s;
+			}
 
-            s >> line.end;
-            if (line.start.x > line.end.x)
-            {
-                std::swap(line.start.x, line.end.x);
-            }
-            if (line.start.y > line.end.y)
-            {
-                std::swap(line.start.y, line.end.y);
-            }
-            return s;
-        }
-    };
+			s >> line.end;
+
+			if (line.start.y > line.end.y)
+			{
+				std::swap(line.start.x, line.end.x);
+				std::swap(line.start.y, line.end.y);
+			}
+			if (line.start.x > line.end.x)
+			{
+				std::swap(line.start.x, line.end.x);
+				std::swap(line.start.y, line.end.y);
+			}
+
+			return s;
+		}
+
+		friend std::ostream& operator<<(std::ostream& o, Line& line) {
+			o << line.start.x << "," << line.start.y << " -> " << line.end.x << "," << line.end.y << std::endl;
+			return o;
+		}
+	};
+
+	void FindPoints(bool flag);
 };
