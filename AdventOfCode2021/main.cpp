b0a50b2e028cfbb88f262ff3685d3dbdc6cacc25
Mingos <domingos_forno@hotmail.com>
2022-08-20 11:15:23 +0100

    Day 7.

--- AdventOfCode2021/main.cpp
+++ AdventOfCode2021/main.cpp
@@ -6,12 +6,13 @@
 //include "Day3.h"
 //#include "Day4.h"
 //#include "Day5.h"
-#include "Day6.h"
+//#include "Day6.h"
+#include "Day7.h"
 	
 int main() {
 
 	auto start = std::chrono::high_resolution_clock::now();
-	Day6 d;
+	Day7 d;
 	d.Solve();
 	auto stop = std::chrono::high_resolution_clock::now();
 	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
